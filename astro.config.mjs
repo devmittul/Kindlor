92bd78efbf023f358b612046a48090ca8ec99b8f
devmittul <devmittul@example.com>
2026-09-21 04:15:56 +0530

    Configure Cloudflare adapter and Wrangler deployment scripts

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,17 @@ import mdx from '@astrojs/mdx';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kindlor.com',
   output: 'static',
   integrations: [react(), mdx(), sitemap()],
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: cloudflare()
 });
